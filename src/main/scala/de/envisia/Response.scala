8fe243825cf405203828562d59878ccc489cdaad
Seung-Zin Nam <z.nam@envisia.de>
2017-10-26 15:26:22 +0200

    parse ipp version correctly

--- src/main/scala/de/envisia/Response.scala
+++ src/main/scala/de/envisia/Response.scala
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets
 import akka.util.ByteString
 import de.envisia.Response.IppResponse
 import de.envisia.util.IppHelper
+import de.envisia.attributes.Attributes._
 
 import scala.annotation.tailrec
 
@@ -12,48 +13,49 @@ class Response(x: ByteString) {
   def getResponse: IppResponse = {
 
     val bb      = x.asByteBuffer
-    val version = IppHelper.bytes2hex(Array(bb.get, bb.get)) // TODO format
+    val version = Array(bb.get, bb.get)(0)
     println(s"Version: $version")
     val statusCode = bb.getShort
     val requestId  = bb.getInt
     println(s"Request ID: $requestId")
+
     @tailrec
     def parseAttributes(groupByte: Byte, attributes: Map[String, List[String]]): Map[String, List[String]] = {
       val byte = bb.get()
 
-      if (byte == 0x03.toByte) {
+      if (byte == ATTRIBUTE_GROUPS("end-of-attributes-tag")) {
         attributes
       } else {
         val (newGroup, attrTag) = {
-          if (byte >= 0x00.toByte && byte <= 0x05.toByte) {
-            // Gruppe
+          if ((0 to 5).contains(byte.toInt)) { // delimiter tag values: https://tools.ietf.org/html/rfc8010#section-3.5.1
+            // group
             val newGroup = byte
-            // Attribut Tag
+            // attribute tag
             val attr = bb.get()
             (newGroup, attr)
           } else {
-            // Attribut Tag
+            // attribute tag
             (groupByte, byte)
           }
         }
 
-        // Name
+        // name
         val shortLenName = bb.getShort()
         val name         = new String(IppHelper.fromBuffer(bb, shortLenName), StandardCharsets.UTF_8)
 
-        // Value
+        // value
         val shortLenValue = bb.getShort()
         val value         = new String(IppHelper.fromBuffer(bb, shortLenValue), StandardCharsets.UTF_8)
 
         val tag = attributes.get(name)
 
-        parseAttributes(newGroup, attributes + (name -> tag.map(v => value :: v).getOrElse(value :: Nil))) // TODO, get sub list !!!
+        parseAttributes(newGroup, attributes + (name -> tag.map(v => value :: v).getOrElse(value :: Nil)))
       }
     }
 
     val attrs = parseAttributes(0x01.toByte, Map.empty) //TODO group byte?
 
-    val result = IppResponse(2, statusCode, requestId, attrs) // TODO see above: get the version dynamically
+    val result = IppResponse(version, statusCode, requestId, attrs)
 
     println(result)
     println(attrs.size)
@@ -64,5 +66,7 @@ class Response(x: ByteString) {
 }
 
 object Response {
+
   case class IppResponse(version: Short, statusCode: Short, requestId: Int, attributes: Map[String, List[String]])
+
 }
