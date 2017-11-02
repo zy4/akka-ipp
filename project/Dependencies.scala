63a3e13c15881e6056312ec176b9d978e81445e5
Seung-Zin Nam <z.nam@envisia.de>
2017-11-02 11:02:40 +0100

    Closes #3: remove unused depdendencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,26 +3,18 @@ import sbt._
 object Dependencies {
 
   val akkaHttpV  = "10.0.10"
-  val circeV     = "0.7.0"
-  val scalaMockV = "3.5.0"
   val scalaTestV = "3.0.3"
   val akkaV      = "2.5.6"
 
   lazy val commonDeps =
     Seq(
-      "org.scalatest"     %% "scalatest"                   % scalaTestV % Test,
-      "com.typesafe.akka" %% "akka-actor"                  % akkaV,
-      "com.typesafe.akka" %% "akka-cluster"                % akkaV,
-      "com.typesafe.akka" %% "akka-cluster-tools"          % akkaV,
-      "com.typesafe.akka" %% "akka-cluster-metrics"        % akkaV,
-      "com.typesafe.akka" %% "akka-slf4j"                  % akkaV,
-      "com.typesafe.akka" %% "akka-stream"                 % akkaV,
-      "com.typesafe.akka" %% "akka-persistence"            % akkaV,
-      "com.typesafe.akka" %% "akka-http"                   % akkaHttpV,
-      "de.heikoseeberger" %% "akka-http-circe"             % "1.15.0",
-      "io.circe"          %% "circe-core"                  % circeV,
-      "io.circe"          %% "circe-generic"               % circeV,
-      "io.circe"          %% "circe-parser"                % circeV
+      "org.scalatest"     %% "scalatest"            % scalaTestV % Test,
+      "com.typesafe.akka" %% "akka-actor"           % akkaV,
+      "com.typesafe.akka" %% "akka-slf4j"           % akkaV,
+      "com.typesafe.akka" %% "akka-stream"          % akkaV,
+      "com.typesafe.akka" %% "akka-http"            % akkaHttpV,
+      "com.typesafe.akka" %% "akka-http-testkit"    % akkaHttpV % Test,
+      "com.typesafe.akka" %% "akka-stream-testkit"  % akkaV % Test
     )
 
 }
