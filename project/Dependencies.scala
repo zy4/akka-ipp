c4551d3a19ef641abd8aab0e8f80397f09ae56c0
Seung-Zin Nam <z.nam@envisia.de>
2017-12-03 20:08:10 +0100

    remove scalatest

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,14 +2,14 @@ import sbt._
 
 object Dependencies {
 
-  val akkaHttpV  = "10.0.11"
-  val scalaTestV = "3.0.3"
-  val akkaV      = "2.5.7"
-  val slf4jV     = "1.7.25"
+  val akkaHttpV = "10.0.11"
+  val uTestV    = "0.6.0"
+  val akkaV     = "2.5.7"
+  val slf4jV    = "1.7.25"
 
   lazy val commonDeps =
     Seq(
-      "com.lihaoyi"       %% "utest"               % "0.6.0" % Test,
+      "com.lihaoyi"       %% "utest"               % uTestV % Test,
       "com.typesafe.akka" %% "akka-actor"          % akkaV,
       "com.typesafe.akka" %% "akka-slf4j"          % akkaV,
       "com.typesafe.akka" %% "akka-stream"         % akkaV,
