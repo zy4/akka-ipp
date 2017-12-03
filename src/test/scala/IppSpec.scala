3ae3d58a9a1efd80e177b552e4782a181b0bc375
Seung-Zin Nam <z.nam@envisia.de>
2017-12-03 19:05:47 +0100

    todo

--- src/test/scala/IppSpec.scala
+++ src/test/scala/IppSpec.scala
@@ -3,15 +3,15 @@ package test
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.{ActorMaterializer, Materializer}
+import de.envisia.akka.ipp.attributes.IPPValue
 import de.envisia.akka.ipp.attributes.IPPValue.TextVal
-import de.envisia.akka.ipp.{IPPClient, IPPConfig}
+import de.envisia.akka.ipp.{IPPClient, IPPConfig, Response}
 import utest._
 
-import scala.concurrent.ExecutionContext
-import scala.util.{Failure, Success}
 
-object IppSpec extends TestSuite{
+import scala.concurrent.{ExecutionContext, Future}
 
+object IppSpec extends TestSuite {
 
   implicit val actorSystem: ActorSystem           = ActorSystem()
   implicit val mat: Materializer                  = ActorMaterializer()
@@ -20,28 +20,23 @@ object IppSpec extends TestSuite{
   val http = Http()
   val client =
     new IPPClient(http)(mat, executionContext)
-  val config = IPPConfig("localhost", port=6632, queue="/printers/cups-pdf")
+  val config = IPPConfig("localhost", port = 6632, queue = "/printers/cups-pdf")
 
+  val pA: Future[Response.GetPrinterAttributesResponse] = client.printerAttributes(config)
+  val response: Future[(Short, Map[String, List[IPPValue]])] = for {
+    r <- pA
+  } yield (r.version, r.attributes)
 
-  var version: Int = -1
-  var naturalLanguageConfigured: String = "foo"
-  client.printerAttributes(config).onComplete {
-    case Success(response) =>
-      version = response.version.toInt
-      naturalLanguageConfigured = response.attributes("natural-language-configured").head.asInstanceOf[TextVal].value
-    case Failure(_) => println("FAILED")
-  }
+  Thread.sleep(2000) // TODO what is the normal way of testing futures here?
 
-  Thread.sleep(2000)
   http.shutdownAllConnectionPools().onComplete(_ => actorSystem.terminate())
 
-  override def tests = Tests{
+  override def tests = Tests {
     'versionTest - {
-      version ==> 2
+      response.map(_._1 ==> 2)
     }
     'someAttrTest - {
-      naturalLanguageConfigured ==> "en-us"
+      response.map(_._2("natural-language-configured").head.asInstanceOf[TextVal].value ==> "en-us")
     }
   }
 }
-
