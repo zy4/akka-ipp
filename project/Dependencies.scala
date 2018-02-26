276a3f192ed952930f72ab797039c9de66e53a82
Seung-Zin Nam <zin@mailbox.org>
2018-02-26 09:58:49 +0100

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val akkaHttpV = "10.0.11"
-  val uTestV    = "0.6.0"
+  val uTestV    = "0.6.3"
   val akkaV     = "2.5.10"
   val slf4jV    = "1.7.25"
   val tikaV     = "1.16"
